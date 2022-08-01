0a53da0d93192d1e0f2789c4adfe05b7550ecb97
Всеволод <83037776+VsevolodPin@users.noreply.github.com>
2022-08-02 02:04:14 +0300

    Реализован поиск минимального расстояния с заданной точностью

--- Geometry/Geometry/Object2D.h
+++ Geometry/Geometry/Object2D.h
@@ -1,9 +1,12 @@
 #pragma once
 #define DllExport __declspec(dllexport)
 #include <math.h>
+#include <vector>
+
+using namespace std;
 
 //  ласс, описывающий вектор в 2D пространстве
-class DllExport Vector2D 
+class DllExport Vector2D
 {
 public:
 	double e1, e2;
@@ -55,7 +58,7 @@ public:
 };
 
 //  ласс, описывающий точку в 2D пространстве
-class  DllExport Point2D 
+class  DllExport Point2D
 {
 public:
 	double e1, e2;
@@ -95,16 +98,18 @@ class  DllExport Curve
 {
 protected:
 	// ћассив точек
-	Point2D* points;
+	vector<Point2D> points;
 public:
 	// “очность моделировани€
 	int accuracy;
 	// ”равнение кривой в параметрическом виде (у каждого наследника будет свое уравнение)
 	virtual Point2D MainFunc(double  t) = 0;
 	// ѕолучение массива точек (Point2D *) кривой
-	virtual Point2D* GetCurvePoints() = 0;
+	virtual vector<Point2D> GetCurvePoints() = 0;
 	// ѕолучение двумерного массива координат точек
-	virtual double** GetCurveCoords() = 0;
+	virtual vector<vector<double>> GetCurveCoords() = 0;
+	// ћетод увеличени€ точности моделировани€ кривой 
+	virtual vector<Point2D> ImproveAccuracy(double t1, double t2, int accuracy) = 0;
 };
 
 //  ласс, представл€ющий кривую Ѕезье 2го пор€дка
@@ -119,7 +124,7 @@ public:
 		this->p2 = p2;
 		this->p3 = p3;
 		this->accuracy = accuracy;
-		points = new Point2D[accuracy];
+		points.resize(accuracy);
 		for (int i = 0; i < accuracy; i++)
 		{
 			points[i] = MainFunc(1.0 / (accuracy - 1) * i);
@@ -127,7 +132,7 @@ public:
 	}
 	~Bezier()
 	{
-		delete[] points;
+		points.clear();
 	}
 	// ”равнение кривой в параметрическом виде 
 	Point2D MainFunc(double  t) override
@@ -135,16 +140,17 @@ public:
 		return p1 * (1 - t) * (1 - t) + p2 * 2 * t * (1 - t) + p3 * t * t;
 	}
 	// ѕолучение массива точек (Point2D *) кривой
-	Point2D* GetCurvePoints()override
+	vector<Point2D> GetCurvePoints()override
 	{
 		return points;
 	}
 	// ѕолучение двумерного массива координат точек
-	double** GetCurveCoords()override
+	vector<vector<double>> GetCurveCoords()override
 	{
-		double** coords = new double* [2];
-		coords[0] = new double[accuracy];
-		coords[1] = new double[accuracy];
+		vector<vector<double>> coords;
+		coords.resize(2);
+		coords[0].resize(accuracy);
+		coords[1].resize(accuracy);
 		for (int i = 0; i < accuracy; i++)
 		{
 			coords[0][i] = points[i].e1;
@@ -152,25 +158,127 @@ public:
 		}
 		return coords;
 	}
+	// ћетод увеличени€ точности моделировани€ кривой 
+	vector<Point2D> ImproveAccuracy(double t1, double t2, int accuracy)override
+	{
+		vector<Point2D> to_return;
+		to_return.resize(accuracy);
+		for (int i = 0; i < accuracy; i++)
+		{
+			to_return[i] = MainFunc(t1 + (t2 - t1) / (accuracy - 1) * i);
+		}
+		return to_return;
+	}
 };
 
-// ‘ункци€ поиска наименьшего рассто€ни€ между массивами точек, представл€ющих собой 2 различные кривые
-DllExport double  FindMinDistance(Point2D* p1, Point2D* p2, int size1, int size2, double eps = 1e-9)
-{
-	Vector2D v = Vector2D(p1[0].e1, p1[0].e2, p2[0].e1, p2[0].e2);
-	double  min = v.GetLength();
+//class DllExport Hermite : public Curve
+//{
+//	Point2D p1, p2;
+//public:
+//	// ƒве опорные точки и точность моделировани€ (по умолчанию 100)
+//	Hermite(Point2D p1, Point2D p2, int accuracy = 100)
+//	{
+//		this->p1 = p1;
+//		this->p2 = p2;
+//		this->accuracy = accuracy;
+//		points = new Point2D[accuracy];
+//		for (int i = 0; i < accuracy; i++)
+//		{
+//			points[i] = MainFunc(1.0 / (accuracy - 1) * i);
+//		}
+//	}
+//	~Hermite()
+//	{
+//		delete[] points;
+//	}
+//	// ”равнение кривой в параметрическом виде 
+//	Point2D MainFunc(double  t) override
+//	{
+//		Point2D m1 = ((p2 - p1) / (p2.e1 - p1.e1) + (p1 / p1.e1)) * 0.5;
+//		Point2D m2 = (p2 / p2.e1) + ((p2 - p1) / (p2.e1 - p1.e1)) * 0.5;
+//		return p1 * (2 * t * t * t - 3 * t * t + 1) + m1 * (t * t * t - 2 * t * t - 1) * (p2.e1 - p1.e1) + p2 * (-2 * t * t * t + 3 * t * t) + m2 * (t * t * t - t * t) * (p2.e1 - p1.e1);
+//	}
+//	// ѕолучение массива точек (Point2D *) кривой
+//	Point2D* GetCurvePoints()override
+//	{
+//		return points;
+//	}
+//	// ѕолучение двумерного массива координат точек
+//	double** GetCurveCoords()override
+//	{
+//		double** coords = new double* [2];
+//		coords[0] = new double[accuracy];
+//		coords[1] = new double[accuracy];
+//		for (int i = 0; i < accuracy; i++)
+//		{
+//			coords[0][i] = points[i].e1;
+//			coords[1][i] = points[i].e2;
+//		}
+//		return coords;
+//	}
+//};
 
-	for (int i = 1; i < size1; i++)
+// ‘ункци€ поиска наименьшего рассто€ни€ между массивами точек, представл€ющих собой 2 различные кривые
+template<typename CurveType1, typename CurveType2> DllExport vector<Point2D> FindClosestPoints(CurveType1 curve1, CurveType2 curve2, double eps = 1e-9)
+{
+	vector<Point2D> to_return;
+	to_return.resize(2);
+	vector<Point2D> closestPoints;
+	closestPoints.resize(2);
+	int idxP1 = 0;
+	int idxP2 = 0;
+	double tFrom1 = 0, tFrom2 = 0;
+	double tTo1 = 1, tTo2 = 1;
+	auto p1 = curve1.GetCurvePoints();
+	auto p2 = curve2.GetCurvePoints();
+	to_return[0] = p1[0];
+	to_return[1] = p2[0];
+	do
 	{
-		for (int j = 0; j < size2; j++)
+		Vector2D v = Vector2D(p1[0].e1, p1[0].e2, p2[0].e1, p2[0].e2);
+		double  min = v.GetLength();
+
+		for (int i = 0; i < curve1.accuracy; i++)
 		{
-			v = Vector2D(p1[0].e1, p1[0].e2, p2[0].e1, p2[0].e2);
-			auto dist = v.GetLength();
-			if (dist < min)
-				min = dist;
-			if (min < 1e-9)
-				return min;
+			for (int j = 0; j < curve2.accuracy; j++)
+			{
+				v = Vector2D(p1[i].e1, p1[i].e2, p2[j].e1, p2[j].e2);
+				auto dist = v.GetLength();
+				if (dist < min)
+				{
+					idxP1 = i;
+					idxP2 = j;
+					closestPoints[0] = p1[i];
+					closestPoints[1] = p2[j];
+					min = dist;
+				}
+			}
 		}
-	}
-	return min;
+
+		double curEps = abs(Vector2D(closestPoints[0].e1, closestPoints[0].e2, closestPoints[1].e1, closestPoints[1].e2).GetLength() - 
+			Vector2D(to_return[0].e1, to_return[0].e2, to_return[1].e1, to_return[1].e2).GetLength());
+		if (curEps < eps)
+		{
+			to_return[0] = closestPoints[0];
+			to_return[1] = closestPoints[1];
+			return to_return;
+		}
+		else
+		{
+			to_return[0] = closestPoints[0];
+			to_return[1] = closestPoints[1];
+			if (idxP1 == 0) idxP1++;
+			if (idxP1 == curve1.accuracy - 1) idxP1--;
+			if (idxP2 == 0) idxP2++;
+			if (idxP2 == curve2.accuracy - 1) idxP2--;
+			double tFromNew1 = ((idxP1 - 1) / (double)(curve1.accuracy-1)) * (tTo1 - tFrom1);
+			double tToNew1 = ((idxP1 + 1) / (double)(curve1.accuracy-1)) * (tTo1 - tFrom1);
+			double tFromNew2 = ((idxP2 - 1) / (double)(curve2.accuracy-1)) * (tTo2 - tFrom2);
+			double tToNew2 = ((idxP2 + 1) / (double)(curve2.accuracy-1)) * (tTo2 - tFrom2);
+			p1 = curve1.ImproveAccuracy(tFromNew1, tToNew1, curve1.accuracy);
+			p2 = curve2.ImproveAccuracy(tFromNew2, tToNew2, curve2.accuracy);
+		}
+
+	} while (true);
+	return to_return;
 }
--- Geometry/Test/Test.cpp
+++ Geometry/Test/Test.cpp
@@ -6,11 +6,15 @@
 
 int main()
 {
-	auto curve1 = Bezier(Point2D(0, 0), Point2D(1, 9), Point2D(10, 10), 1000);
-	auto curve2 = Bezier(Point2D(1, 0), Point2D(10, 1), Point2D(11, 10), 1000);
+	auto curve1 = Bezier(Point2D(5, 5), Point2D(1, 9), Point2D(15, 15), 100);
+	auto curve2 = Bezier(Point2D(1, 0), Point2D(10, 1), Point2D(11, 10), 100);
 	auto test = curve1.GetCurveCoords();
 
-	auto minDist = FindMinDistance(curve1.GetCurvePoints(), curve2.GetCurvePoints(), 1000, 1000);
-
-	std::cout << "Min distance between curve1 and curve2 = " << minDist << "\n";
+	auto closestPoints = FindClosestPoints<Bezier, Bezier>(curve1, curve2, 1e-9);
+	std::cout.precision(9);
+	std::cout << "Min distance between curve1 and curve2 = " <<
+		closestPoints[0].e1 << " " <<
+		closestPoints[0].e2 << " " <<
+		closestPoints[1].e1 << " " <<
+		closestPoints[1].e2 << "\n";
 }
