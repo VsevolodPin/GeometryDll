fa717eb45183ca01cfb88c8c5d2f8a2590ba3395
Всеволод <83037776+VsevolodPin@users.noreply.github.com>
2022-08-04 02:25:18 +0300

    Подправил комментарии

---
+++ Geom/Geom/Form1.cs
@@ -0,0 +1,127 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+
+namespace Geom
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        public double FindMinDistance(List<Point2D> p1, List<Point2D> p2, out int idxP1, out int idxP2)
+        {
+            double min = new Vector2D(p1[0], p2[0]).GetLength();
+            idxP1 = 0;
+            idxP2 = 0;
+
+            for (int i = 0; i < p1.Count; i++)
+            {
+                for (int j = 0; j < p1.Count; j++)
+                {
+                    var dist = new Vector2D(p1[i], p2[j]).GetLength();
+                    if (dist < min)
+                    {
+                        idxP1 = i;
+                        idxP2 = j;
+                        min = dist;
+                    }
+                }
+            }
+            return min;
+        }
+
+        public Point2D FindCrossPoint(Curve curve1, Curve curve2, double eps)
+        {
+            double prevLength = double.MaxValue, curLength = 0;
+            double curEps = 0.1;
+            List<Point2D> arr1 = curve1.points;
+            List<Point2D> arr2 = curve2.points;
+            do
+            {
+                int idxP1, idxP2;
+                curLength = FindMinDistance(arr1, arr2, out idxP1, out idxP2);
+                if (idxP1 == 0) idxP1++;
+                if (idxP2 == 0) idxP2++;
+                if (idxP1 == arr1.Count - 1) idxP1--;
+                if (idxP2 == arr2.Count - 1) idxP2--;
+
+                if (curLength < prevLength)
+                {
+                    if ((curve1.points[idxP1 - 1].e2 - curve2.points[idxP2 - 1].e2) * (curve1.points[idxP1 + 1].e2 - curve2.points[idxP2 + 1].e2) < 0)
+                    {
+                        curEps = prevLength - curLength;
+                        if (curEps < 0) curEps *= -1;
+                        if (curEps < eps)
+                        {
+                            return (curve1.points[idxP1] + curve2.points[idxP2]) / 2;
+                        }
+                        else
+                        {
+                            prevLength = curLength;
+                            arr1 = curve1.ImproveAccuracy(((double)(idxP1 - 1)) / curve1.accuracy, ((double)(idxP1 + 1)) / curve1.accuracy, 100);
+                            arr2 = curve2.ImproveAccuracy(((double)(idxP2 - 1)) / curve2.accuracy, ((double)(idxP2 + 1)) / curve2.accuracy, 100);
+                        }
+                    }
+                }
+            } while (true);
+            return new Point2D(0, 0);
+        }
+
+        private void Button1_Click(object sender, EventArgs e)
+        {
+            //var curve1 = new Bezier(new Point2D(5, 5), new Point2D(1, 9), new Point2D(15, 15), 100);
+            //var curve2 = new Bezier(new Point2D(1, 0), new Point2D(10, 1), new Point2D(11, 10), 100);
+            var curve1 = new Bezier(new Point2D[] { new Point2D(3, 0), new Point2D(1, 3), new Point2D(5, 5), new Point2D(7, 3) }, 100);
+            var curve2 = new Bezier(new Point2D[] { new Point2D(2, 0), new Point2D(6, 3), new Point2D(10, 10) }, 100);
+
+            var points1 = curve1.GetCurveCoords();
+            var points2 = curve2.GetCurveCoords();
+
+            int i, j;
+
+            var minDist = FindMinDistance(curve1.GetCurvePoints(), curve2.GetCurvePoints(), out i, out j);
+            //var crossPoint = FindCrossPoint(curve1, curve2, 1e-3);
+
+            //var p1 = curve1.ImproveAccuracy((double)(i - 2) / curve1.accuracy, (double)(i + 2) / curve1.accuracy, 100);
+            //var p2 = curve2.ImproveAccuracy((double)(j - 2) / curve1.accuracy, (double)(j + 2) / curve1.accuracy, 100);
+
+            //double[][] coords1 = new double[2][];
+            //double[][] coords2 = new double[2][];
+            //coords1[0] = new double[p1.Count];
+            //coords1[1] = new double[p1.Count];
+            //coords2[0] = new double[p2.Count];
+            //coords2[1] = new double[p2.Count];
+            //for (int k = 0; k < p1.Count; k++)
+            //{
+            //    coords1[0][k] = p1[k].e1;
+            //    coords1[1][k] = p1[k].e2;
+            //    coords2[0][k] = p2[k].e1;
+            //    coords2[1][k] = p2[k].e2;
+            //}
+
+            DrawerCollection d = new DrawerCollection();
+
+            DRWDrawer drawer = d.DRWCreator(pictureBox1, true, true, "");
+
+            drawer.axis_countX = 11;
+            drawer.axis_countY = 11;
+            drawer.AxisConfig(new int[] { 11, 11 });
+
+            drawer.Resize(new double[] { 0, 10, 0, 10 });
+            //drawer.DrawGraph(coords1[0], coords1[1], false);
+            //drawer.AddGraph(coords2[0], coords2[1]);
+            drawer.DrawGraph(curve1.GetCurveCoords()[0], curve1.GetCurveCoords()[1], false);
+            drawer.AddGraph(curve2.GetCurveCoords()[0], curve2.GetCurveCoords()[1]);
+        }
+    }
+}
---
+++ Geom/Geom/Objects.cs
@@ -0,0 +1,194 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Geom
+{
+    public class Point2D
+    {
+        public double e1, e2;
+        public Point2D()
+        {
+            e1 = 0;
+            e2 = 0;
+        }
+        public Point2D(double e1, double e2)
+        {
+            this.e1 = e1;
+            this.e2 = e2;
+        }
+
+
+        public static Point2D operator +(Point2D p1, Point2D p2)
+        {
+            return new Point2D(p1.e1 + p2.e1, p1.e2 + p2.e2);
+        }
+        public static Point2D operator -(Point2D p1, Point2D p2)
+        {
+            return new Point2D(p1.e1 - p2.e1, p1.e2 - p2.e2);
+        }
+        public static Point2D operator *(Point2D p, double a)
+        {
+            return new Point2D(p.e1 * a, p.e2 * a);
+        }
+        public static Point2D operator /(Point2D p, double a)
+        {
+            return new Point2D(p.e1 / a, p.e2 / a);
+        }
+    }
+
+    public class Vector2D
+    {
+        double e1, e2;
+        public Vector2D()
+        {
+            e1 = 0;
+            e2 = 0;
+        }
+        public Vector2D(double e1, double e2)
+        {
+            this.e1 = e1;
+            this.e2 = e2;
+        }
+
+        public Vector2D(Point2D p1, Point2D p2)
+        {
+            this.e1 = p1.e1 - p2.e1;
+            this.e2 = p1.e2 - p2.e2;
+        }
+
+        public static Vector2D operator +(Vector2D v1, Vector2D v2)
+        {
+            return new Vector2D(v1.e1 + v2.e1, v1.e2 + v2.e2);
+        }
+        public static Vector2D operator -(Vector2D v1, Vector2D v2)
+        {
+            return new Vector2D(v1.e1 - v2.e1, v1.e2 - v2.e2);
+        }
+        public static Vector2D operator *(Vector2D v, double a)
+        {
+            return new Vector2D(v.e1 * a, v.e2 * a);
+        }
+        public static double operator *(Vector2D v1, Vector2D v2)
+        {
+            return v1.e1 * v2.e1 + v1.e2 * v2.e2;
+        }
+        public double GetLength()
+        {
+            return Math.Sqrt(e1 * e1 + e2 * e2);
+        }
+    };
+
+    public abstract class Curve
+    {
+        public int accuracy;
+        public List<Point2D> points;
+        public abstract Point2D MainFunc(double t);
+        public abstract List<Point2D> ImproveAccuracy(double t1, double t2, int accuracy);
+    }
+
+    public class Bezier : Curve
+    {
+        public Point2D p1, p2, p3;
+        public Point2D[] basePoints;
+
+        public Bezier(Point2D p1, Point2D p2, Point2D p3, int accuracy = 10)
+        {
+            this.p1 = p1;
+            this.p2 = p2;
+            this.p3 = p3;
+            this.accuracy = accuracy;
+            points = new List<Point2D>();
+            for (int i = 0; i < accuracy; i++)
+            {
+                var res = MainFunc(1.0 / (accuracy - 1) * i);
+                points.Add(res);
+            }
+        }
+        public Bezier(Point2D[] basePoints, int accuracy = 10)
+        {
+            this.basePoints = basePoints;
+            this.accuracy = accuracy;
+            points = new List<Point2D>();
+            for (int i = 0; i < accuracy; i++)
+            {
+                var res = MainFunc2(1.0 / (accuracy - 1) * i);
+                points.Add(res);
+            }
+        }
+
+        public override Point2D MainFunc(double t)
+        {
+            var P1 = p1 * (1 - t) * (1 - t);
+            var P2 = p2 * 2 * t * (1 - t);
+            var P3 = p3 * t * t;
+            var to_return = P1 + P2 + P3;
+            return to_return;
+        }
+
+        public int fact(int n)
+        {
+            if (n <= 1) return 1;
+            int to_return = 1;
+            for (int i = n; i > 0; i--)
+            {
+                to_return *= i;
+            }
+            return to_return;
+        }
+
+        public Point2D MainFunc2(double t)
+        {
+            Point2D to_return = new Point2D(0, 0);
+            int n = basePoints.Length - 1;
+            for (int i = 0; i < basePoints.Length; i++)
+            {
+                if (i != 0 && n - i != 0)
+                {
+                    double N = fact(n);
+                    double K = fact(i);
+                    double N_K = fact(n - i);
+                    to_return = to_return + basePoints[i] * Math.Pow(t, i) * Math.Pow(1 - t, n - i) * (double)(fact(n)) / (fact(i) * fact(n - i));
+                }
+                else
+                {
+                    if (i == 0)
+                        to_return = to_return + basePoints[i] * 1 * Math.Pow(1 - t, n - i) * (double)(fact(n)) / (fact(i) * fact(n - i));
+                    if (n - i == 0)
+                        to_return = to_return + basePoints[i] * Math.Pow(t, i) * 1 * (double)(fact(n)) / (fact(i) * fact(n - i));
+                }
+
+            }
+            return to_return;
+        }
+
+        public List<Point2D> GetCurvePoints()
+        {
+            return points;
+        }
+        public double[][] GetCurveCoords()
+        {
+            double[][] coords = new double[2][];
+            coords[0] = new double[accuracy];
+            coords[1] = new double[accuracy];
+            for (int i = 0; i < accuracy; i++)
+            {
+                coords[0][i] = points[i].e1;
+                coords[1][i] = points[i].e2;
+            }
+            return coords;
+        }
+        public override List<Point2D> ImproveAccuracy(double t1, double t2, int accuracy)
+        {
+            List<Point2D> to_return = new List<Point2D>();
+            for (int i = 0; i < accuracy; i++)
+            {
+                to_return.Add(MainFunc(t1 + (t2 - t1) / (accuracy - 1) * i));
+            }
+            return to_return;
+        }
+    }
+}
+
